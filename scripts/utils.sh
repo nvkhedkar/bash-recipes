92cb110db8351dc2b6e1ec483d21b5610b01d0c1
Khedkar <nkhedkar@ptc.com>
2020-12-20 19:37:22 +0530

    nvk - logrotate for airflow

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+LOG_FILE=~/bash_utils.log
+echo "setup logs" > $LOG_FILE
+log()
+{
+  MESSAGE=$1
+  if [ $# -eq 1 ]; then
+	  echo "$MESSAGE"
+	fi
+  echo "$MESSAGE" >> $LOG_FILE
+#    echo "`date` :: $MESSAGE" >> $LOG_FILE
+}
+
 #==============================================================================
 # Run command functions
 #==============================================================================
@@ -58,7 +70,7 @@ create_user(){
     run_cmd "sudo groupadd $usr"
     # -G rabbitmq,kibana # -c 'ptc $usr user'
     log "running sudo useradd"
-    CMDRES=$(sudo useradd $usr -m -s /bin/bash -g $usr -c "Generative user")
+    CMDRES=$(sudo useradd $usr -m -s /bin/bash -g $usr -c "New user")
     log "$CMDRES"
     echo -e "$usrpass\n$usrpass" | (sudo passwd $usr)
     if [[ -d "/home/$usr" ]]; then
@@ -69,7 +81,7 @@ create_user(){
 
   elif [[ $typ = "system" ]]; then
     run_cmd "sudo groupadd $usr"
-    CMDRES=$(sudo useradd -r $usr -g $usr -s /bin/bash -c "Generative user")
+    CMDRES=$(sudo useradd -r $usr -g $usr -s /bin/bash -c "New user")
     log "$CMDRES"
   fi
 
@@ -82,3 +94,30 @@ create_user(){
   fi
   run_cmd "groups $usr"
 }
+
+
+setup_rsyslog()
+{
+  local RSYSLOG_DIR=/var/log/myproject
+  local MY_RSYS_CONF=~/myproject_conf
+  local RSYSLOG_CONF_FILE=myproject_rsyslog.conf
+  show_title "Setup rsyslog"
+  run_cmd_simple "sudo mkdir -p $RSYSLOG_DIR"
+  run_cmd_simple "sudo chown -R syslog:adm $RSYSLOG_DIR"
+  run_cmd_simple "sudo rm /etc/rsyslog.d/$RSYSLOG_CONF_FILE"
+  run_cmd_simple "sudo ln -s $MY_RSYS_CONF/$RSYSLOG_CONF_FILE /etc/rsyslog.d/$RSYSLOG_CONF_FILE"
+  run_cmd_simple "sudo systemctl restart rsyslog"
+}
+
+
+setup_logrotate()
+{
+  local myproject_CONF=~/myproject_conf
+  local MY_LOGROTATE=myproject-logrotate
+  show_title "logrotate configuration"
+  run_cmd_simple "sudo chown root:root $myproject_CONF_DIR/$MY_LOGROTATE"
+  run_cmd_simple "sudo rm /etc/logrotate.d/$MY_LOGROTATE"
+  run_cmd_simple "sudo ln -s $myproject_CONF_DIR/$MY_LOGROTATE /etc/logrotate.d/$MY_LOGROTATE"
+  run_cmd_simple "sudo chmod 0644 /etc/logrotate.d/$MY_LOGROTATE"
+}
+
